c988cbd6c7396a894b1e4f64e5fa7125fb1e3a16
Sarthak Budhiraja <sbudhiraja@shutterstock.com>
2018-08-19 23:10:46 +0530

    spacing

--- test/googletest.js
+++ test/googletest.js
@@ -4,8 +4,8 @@ var chai = require('chai');
 describe('Testing google.com', () => {
     it('should return 200', (done) => {
 	    request("http://www.google.com", (error, response, body) => {
-		  chai.assert.equal(response.statusCode, 200);
-		  done();
-		});
+		    chai.assert.equal(response.statusCode, 200);
+		    done();
+		  });
     });
 });
