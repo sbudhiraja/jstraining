1a33011ee05c3df7294edc2103477fd4477af1e0
sbudhiraja <42519843+sbudhiraja@users.noreply.github.com>
2018-08-19 23:13:47 +0530

    good spacing

--- test/googletest.js
+++ test/googletest.js
@@ -2,10 +2,10 @@ var request = require('request');
 var chai = require('chai');
 
 describe('Testing google.com', () => {
-    it('should return 200', (done) => {
-	    request("http://www.google.com", (error, response, body) => {
-		    chai.assert.equal(response.statusCode, 200);
-		    done();
-		  });
+  it('should return 200', (done) => {
+    request("http://www.google.com", (error, response, body) => {
+      chai.assert.equal(response.statusCode, 200);
+      done();
     });
+  });
 });
